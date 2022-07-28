dfd7b8fb686cc33055db21ccca2a9876dfaf0387
Jose Miguel Alcaraz <90214208+jose1up@users.noreply.github.com>
2022-07-28 00:10:49 -0300

    fixin variable

--- client/src/redux/action/index.js
+++ client/src/redux/action/index.js
@@ -1,11 +1,13 @@
 import axios from "axios";
-const baseURL = process.env.REACT_APP_API;
+import "dotenv/config";
+const baseURL = process.env.REACT_APP_API_URL;
 export const GET_TEXT = "GET_TEXT";
 
 export const getText = (text) => {
   try {
     return async (dispatch) => {
-      const { data } = await axios.get(`http://localhost:3001/iecho?text=${text}`);
+      console.log(baseURL);
+      const { data } = await axios.get(`${baseURL}/iecho?text=${text}`);
       return dispatch({
         type: GET_TEXT,
         payload: data,
